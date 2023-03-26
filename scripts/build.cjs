687f5b08124598c39e6ec41230953f17be91041e
hlccare <280108904@qq.com>
2023-03-26 21:05:18 +0800

    build: 增加按需打包功能

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -11,6 +11,9 @@ const baseConfig = defineConfig({
 });
 const entryFile = path.resolve(__dirname, "./entry.ts");
 
+// 组件目录
+const componentsDir = path.resolve(__dirname, "../src");
+
 const outputDir = path.resolve(__dirname, "../dist");
 
 // rollup配置
@@ -24,14 +27,29 @@ const rollupOptions = {
   },
 };
 
-const createPackageJson = () => {
+const createPackageJson = (name) => {
+  const packageName = name ?? "simple-ui";
+  const fileName = name ? "index" : "simple-ui";
   const fileStr = `{
-  "name": "simple-ui",
+  "name": "${packageName}",
   "version": "0.0.0",
-  "main": "simple-ui.umd.cjs",
-  "module": "simple-ui.esm.js"
+  "main": "${fileName}.umd.cjs",
+  "module": "${fileName}.esm.js"
 }`;
-  fsExtra.outputFile(path.resolve(outputDir, "package.json"), fileStr, "utf-8");
+  if (name) {
+    // 单组件package.json
+    fsExtra.outputFile(
+      path.resolve(outputDir, `${name}/package.json`),
+      fileStr,
+      "utf-8"
+    );
+  } else {
+    fsExtra.outputFile(
+      path.resolve(outputDir, "package.json"),
+      fileStr,
+      "utf-8"
+    );
+  }
 };
 
 // 全量构建
@@ -51,11 +69,43 @@ const buildAll = async () => {
       },
     })
   );
-};
-
-const buiidLib = async () => {
-  await buildAll();
   createPackageJson();
 };
 
-buiidLib();
+// 按需打包
+const buildSingle = async (name) => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        rollupOptions,
+        lib: {
+          entry: path.resolve(componentsDir, name),
+          name: "index",
+          fileName: "index",
+          formats: ["esm", "umd"],
+        },
+        outDir: path.resolve(outputDir, name),
+      },
+    })
+  );
+  createPackageJson(name);
+};
+
+const buildLib = async () => {
+  await buildAll();
+
+  // 按需
+  fsExtra
+    .readdirSync(componentsDir)
+    .filter((name) => {
+      const componentDir = path.resolve(componentsDir, name);
+      const isDir = fsExtra.lstatSync(componentDir).isDirectory();
+      return isDir && fsExtra.readdirSync(componentDir).includes("index.ts");
+    })
+    .forEach(async (name) => {
+      await buildSingle(name);
+    });
+};
+
+buildLib();
